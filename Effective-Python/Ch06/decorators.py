bd30a60aa6ae4f33b7b1029f25d0ad4065bf0419
Zachary King <kingzach77@gmail.com>
2016-02-29 22:40:38 -0600

    Effective Python

---
+++ Effective-Python/Ch06/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+
+def trace(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print('%s(%r, %r) -> %r' %
+            (func.__name__, args, kwargs, result))
+        return result
+    return wrapper
+
+@trace
+def fibonacci(n):
+    """Returns the n-th Fibonacci number"""
+    if n in (0, 1):
+        return n
+    return (fibonacci(n - 2) + fibonacci(n - 1))
+
+if __name__ == "__main__":
+    fibonacci(3)
+
---
+++ Effective-Python/README.md
@@ -0,0 +1,5 @@
+# Effective Python - Code
+This directory houses some of the code I wrote while 
+reading the book, "Effective Python: 59 Ways to Write Better Python" 
+by Brett Slatkin.
+
