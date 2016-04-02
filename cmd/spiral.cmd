51e20d192fbff2539485e0792133d48eda645110
Zachary King <kingzach77@gmail.com>
2016-04-02 18:40:13 -0500

    Turtle Shell

---
+++ cmd/spiral.cmd
@@ -0,0 +1,30 @@
+position
+heading
+reset
+circle 20
+right 30
+circle 40
+right 30
+circle 60
+right 30
+circle 80
+right 30
+circle 100
+right 30
+circle 120
+right 30
+circle 120
+heading
+forward 100
+
+right 90
+forward 100
+
+right 90
+forward 400
+right 90
+forward 500
+right 90
+forward 400
+right 90
+forward 300
