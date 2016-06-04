4f76a8ab4941f99be590b50e7b4c72c34fcff3ee
Zachary King <kingzach77@gmail.com>
2016-06-04 11:12:29 -0500

    Simple Echo Server/Client from Python docs

---
+++ Networking/echo-server.py
@@ -0,0 +1,18 @@
+# Echo server program
+import socket
+
+HOST = ''
+PORT = 50007
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+
+s.listen(1)         # listen for maximum 1 connection
+conn, addr = s.accept()
+print('Connected by', addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data: break
+    conn.sendall(data)
+conn.close()
