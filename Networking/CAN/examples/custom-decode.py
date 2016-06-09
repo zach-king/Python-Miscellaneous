0c5d11bfd7599dd6941064bbf2033fc25095c5b0
Zachary King <kingzach77@gmail.com>
2016-06-09 13:07:55 -0500

    Authentication frame example, and custom frame db

---
+++ Networking/CAN/examples/custom-decode.py
@@ -0,0 +1,22 @@
+from canard import can, bus
+from canard.file import jsondb
+from canard.hw import socketcan
+import sys
+
+if len(sys.argv) != 3:
+    print('Usage:\n\tpython3 decode.py <dev-name> <db-file>')
+    sys.exit(1)
+
+parser = jsondb.JsonDbParser()
+b = parser.parse(sys.argv[2])
+
+dev = socketcan.SocketCanDev(sys.argv[1])
+dev.start()
+
+while True:
+    frame = dev.recv()
+    signals = b.parse_frame(frame)
+    if signals:
+        for s in signals:
+            print(s)
+        print('---------------------------------------------------')
