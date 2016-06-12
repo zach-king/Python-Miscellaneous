0b55b5b9b8b5654cabb9da35667c5823f1dbc2da
Zachary King <kingzach77@gmail.com>
2016-06-12 15:00:32 -0500

    Reading Serial CAN messages from Arduino - initial finished

--- Arduino/read-can.py
+++ Arduino/read-can.py
@@ -1,18 +1,25 @@
 import serial
+import time
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.readline() # 'Press 1 to LED ON...'
 
 while True:
-    _id = str(ser.readline(), 'utf-8').rstrip() 
-    _dlc = str(ser.readline(), 'utf-8').rstrip()
-    ser.readline()
-    _data = str(ser.readline(), 'utf-8').rstrip()
-    ser.readline()
-    ser.readline()
+    time.sleep(1)
+    data_avail = ser.inWaiting()
+    #print(str(data_avail))
+    msg_str = str(ser.readline(), 'utf-8').rstrip() 
+    print('msg:', msg_str)
+    _id, msg_str = msg_str.split('#')
+    _dlc = msg_str[:2]
+    _data = msg_str[2:]
 
-    print(_id)
-    print(_dlc)
-    print(_data)
-    print("------------------")
+    _id = int('0' + _id, 16)
+    _dlc = int(_dlc, 16)
+    _data = [_data[i:i+2] for i in range(0, len(_data), 2)]
+    _data = [int(i, 16) for i in _data]
+
+    print('ID:',_id)
+    print('dlc:', _dlc)
+    print('data:', _data)
+    print('-------------------------')
 
