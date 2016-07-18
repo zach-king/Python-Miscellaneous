b17fa2408346c5431d31a93a6bc4847e7a8a90cf
Zachary King <kingzach77@gmail.com>
2016-07-18 17:30:04 -0500

    Updated header docstring with example

--- Cryptography/cryptopress.py
+++ Cryptography/cryptopress.py
@@ -9,6 +9,15 @@ Description: Cryptopress translates to 'cryptography compression.'
     output the ciphertext to a file--an archive. Then you can
     use this program to do the inverse action and
     produce the original file(s) from the archive file.
+
+    For example, to encrypt ALL the files recursively inside 
+    the current working directory, output the ciphertext to 
+    'archive.enc', and DELETE ALL of the original files,
+    do this (uses the key 'shrubbery'):
+    python cryptopress.py shrubbery -f * -o archive.enc -d
+
+    Then, to get all the original files back from the archive:
+    python cryptopress.py shrubbery -r archive.enc
 """
 
 from Crypto import Random
